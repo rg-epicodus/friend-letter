81224c96ff3ade3fc8518c918660e8af7713ac4f
Rich Garrick <richg341@gmail.com>
2017-08-06 21:08:04 -0700

    add initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import java.util.Map;
+import static spark.Spark.*;
+
+public class App {
+
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        get("/", (request, response) -> { //request for route happens at this location
+            Map<String, Object> model = new HashMap<String, Object>(); // new model is made to store information
+            return new ModelAndView(model, "hello.hbs"); // assemble individual pieces and render
+        }, new HandlebarsTemplateEngine()); //
+
+        get("/favorite_photos", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "favorite_photos.hbs");
+        }, new HandlebarsTemplateEngine());
+
+    }
+}
